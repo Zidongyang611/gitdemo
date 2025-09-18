c14849739013b80cf19cf2bb2ef0ecd44e703902
Zidongyang611 <1656372954@qq.com>
2025-09-18 11:10:31 +0800

    Revert "5"
    This reverts commit 7b83ab6b5a90ad500abc1351e1ed5be9d1f47a6f.

--- testgit.py
+++ testgit.py
@@ -4,5 +4,4 @@ print("hello world")
 print("hello world3")
 print("hello world4")
 print("git的学习,现在已经更改了代理，不知道是否能成功呢")
-print("改了代理，让我关闭代理上传试一下，看看是否能成功呢，原来也能成功")
-单独wwan
+print("改了代理，让我关闭代理上传试一下，看看是否能成功呢")
